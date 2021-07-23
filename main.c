29a9bddd90ed3be726c40468bf1bba37d0f66a1c
swetha-1011 <87572963+swetha-1011@users.noreply.github.com>
2021-07-23 19:06:45 +0530

    c program

---
+++ main.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+   char ch;
+
+   scanf("%c",&ch);
+
+   switch(ch) {
+
+   case 'B': printf("Burger");
+
+                  break;
+
+   case 'F': printf("French Fries");
+
+                  break;
+
+   case 'S': printf("Sandwich");
+
+                  break;
+
+   case 'P': printf("Pizza");
+
+}
+
+   return 0;
+
+}
+
+
