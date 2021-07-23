b09c4b12c53d257122a0272123a1509e10671abc
swetha-1011 <87572963+swetha-1011@users.noreply.github.com>
2021-07-23 19:29:37 +0530

    sum of Digits

--- main.c
+++ main.c
@@ -6,34 +6,31 @@ C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-
 #include <stdio.h>
+
+int sum(int n)  {
+
+   if(n!=0)
+
+       return (n%10 + sum(n/10));
+
+   else
+
+       return 0;
+
+}
 
 int main()
 
 {
 
-   char ch;
+   int n;
 
-   scanf("%c",&ch);
+   printf("Enter a number - ");
 
-   switch(ch) {
+   scanf("%d",&n);
 
-   case 'B': printf("Burger");
-
-                  break;
-
-   case 'F': printf("French Fries");
-
-                  break;
-
-   case 'S': printf("Sandwich");
-
-                  break;
-
-   case 'P': printf("Pizza");
-
-}
+   printf("Sum of the digits of %d is: %d",n,sum(n));
 
    return 0;
 
