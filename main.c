24c41ba15a1edd8dd306e8983501df2333bc0042
swetha-1011 <87572963+swetha-1011@users.noreply.github.com>
2021-07-26 18:57:59 +0530

    Grade program

--- main.c
+++ main.c
@@ -7,45 +7,39 @@ Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include<stdio.h>
-
-#include<conio.h>
-
-int lcm(int a , int b)
-
+void main()
 {
-
-static int m = 0;
-
-m = m + b;
-
-if(m % a == 0 && m % b == 0)
-
-{
-
- return m;
-
+    int marks;
+    printf("Enter your marks ");
+    scanf("%d",&marks);
+    if(marks<0 || marks>100)
+    {
+        printf("Wrong Entry");
+    }
+    else if(marks<50)
+    {
+        printf("Grade F");
+    }
+    else if(marks>=50 && marks<60)
+    {
+        printf("Grade D");
+    }
+    else if(marks>=60 && marks<70)
+    {
+        printf("Grade C");
+    }
+    else if(marks>=70 && marks<80)
+    {
+        printf("Grade B");
+    }
+    else if(marks>=80 && marks<90)
+    {
+        printf("Grade A");
+    }
+    else
+    {
+        printf("Grade A+");
+    }
 }
+		
 
-return lcm(a , b);
-
-}
-
-int main()
-
-{
-
-int x , y;
-
-printf("Enter First Number : ");
-
-scanf("%d" , &x);
-
-printf("Enter Seconf Number : ");
-
-scanf("%d" , &y);
-
-printf("LCM of %d and %d = %d" , x , y , lcm(x , y));
-
-return 0;
-
-}
