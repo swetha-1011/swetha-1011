c95c36851ad99f3596d1dad96afb5492c38e3e70
swetha-1011 <87572963+swetha-1011@users.noreply.github.com>
2021-07-27 18:57:50 +0530

    Structure Employee

--- main.c
+++ main.c
@@ -6,40 +6,30 @@ C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-#include<stdio.h>
-void main()
+#include <stdio.h>
+ 
+/*structure declaration*/
+struct employee{
+    char    name[30];
+    int     empId;
+    float   salary;
+};
+ 
+int main()
 {
-    int marks;
-    printf("Enter your marks ");
-    scanf("%d",&marks);
-    if(marks<0 || marks>100)
-    {
-        printf("Wrong Entry");
-    }
-    else if(marks<50)
-    {
-        printf("Grade F");
-    }
-    else if(marks>=50 && marks<60)
-    {
-        printf("Grade D");
-    }
-    else if(marks>=60 && marks<70)
-    {
-        printf("Grade C");
-    }
-    else if(marks>=70 && marks<80)
-    {
-        printf("Grade B");
-    }
-    else if(marks>=80 && marks<90)
-    {
-        printf("Grade A");
-    }
-    else
-    {
-        printf("Grade A+");
-    }
+    /*declare structure variable*/
+    struct employee emp;
+     
+    /*read employee details*/
+    printf("\nEnter details :\n");
+    printf("Name ?:");          gets(emp.name);
+    printf("ID ?:");            scanf("%d",&emp.empId);
+    printf("Salary ?:");        scanf("%f",&emp.salary);
+     
+    /*print employee details*/
+    printf("\nEntered detail is:");
+    printf("Name: %s"   ,emp.name);
+    printf("Id: %d"     ,emp.empId);
+    printf("Salary: %f\n",emp.salary);
+    return 0;
 }
-		
-
